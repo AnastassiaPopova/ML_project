38dbe6e9c0298d14c08680a478164ae6f2561069
AnastassiaPopova <135708120+AnastassiaPopova@users.noreply.github.com>
2023-06-30 22:46:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-path="/model_gr.pickle"
+#path="/model_gr.pickle"
+path = "/app/ml_project/model_gr.pickley"
 
 #with open('path', 'rb') as f:
  #   model = pickle.load(path)
