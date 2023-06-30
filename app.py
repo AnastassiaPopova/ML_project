55ab1571db7c17de505d7c51b168c9052da2ea87
AnastassiaPopova <135708120+AnastassiaPopova@users.noreply.github.com>
2023-07-01 00:14:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 
-with open('model_gr.pkl', 'rb') as handle:
+with open('model_gr.pickle', 'rb') as handle:
     model = pickle.load(handle)
 
 st.title('Предсказание цены подержанного автомобиля')
