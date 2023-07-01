c73b472e24c79b7c4ca02c4af6b7199a87fd3932
AnastassiaPopova <135708120+AnastassiaPopova@users.noreply.github.com>
2023-07-01 18:27:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ with open('model_gr.pickle', 'rb') as handle:
 
 st.title('Предсказание цены подержанного автомобиля')
 
-year = int(st.text_input("Год выпуска машины", "1234"))
+year = int(st.text_input("Год выпуска машины", "2015"))
 km_driven = st.number_input('Пробег машины (км)')
-fuel = st.select_slider('Тип топлива', ['Дизель', 'Бензин', 'Другое'])
+fuel = st.sidebar.select_slider('Тип топлива', ['Дизель', 'Бензин', 'Другое'])
 if fuel == "Бензин":
     fuel_Petrol = 1
     fuel_Others = 0
@@ -25,17 +25,17 @@ seats = st.slider("Количество мест", 2, 14)
 engine = st.number_input('Объем двигателя (куб.см)')
 max_power = st.number_input('Пиковая мощность (л.с.)')
 mileage = st.number_input('Расход двигателя (км/л)')
-seller_type = st.selectbox("Тип продавца", ("Индивидуальный", "Дилер"))
+seller_type = st.sitebar.selectbox("Тип продавца", ("Индивидуальный", "Дилер"))
 if seller_type == "Индивидуальный":
     seller_type_Individual = 1
 else:
     seller_type_Individual = 0
-transmission = st.selectbox("Коробка передач", ("Механическая", "Автоматическая"))
+transmission = st.sitebar.selectbox("Коробка передач", ("Механическая", "Автоматическая"))
 if transmission == "Механическая":
     transmission_Manual = 1
 else:
     transmission_Manual = 0
-owner = st.selectbox("Первый владелец", ("Да", "Нет"))
+owner = st.sidebar.selectbox("Первый владелец", ("Да", "Нет"))
 if owner == "Нет":
     owner_Second = 1
 else:
@@ -46,9 +46,7 @@ def predict():
                    fuel_Others, fuel_Petrol, seller_type_Individual, transmission_Manual, owner_Second])
     X = pd.DataFrame(row.reshape(1, -1))
     prediction = model.predict(X)
-    st.write(prediction[0])
     st.write(np.exp(prediction)[0])
-    st.write(row)
 
 st.button('Рассчитать стоимость', on_click = predict)
 st.write(row)
