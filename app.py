022d438f87af94423f4e221c017288483a631bd4
AnastassiaPopova <135708120+AnastassiaPopova@users.noreply.github.com>
2023-07-01 21:54:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -63,9 +63,8 @@ else:
 def predict():
     row = np.array([year, km_driven, engine, max_power, seats, mileage, \
                    fuel_Others, fuel_Petrol, seller_type_Individual, transmission_Manual, owner_Second])
-    X = pd.DataFrame(row.reshape(1, -1))
-    prediction = model.predict(X)
-    st.write(np.exp(prediction)[0])
+    prediction = model.predict(row.reshape(1, -1))
+    st.write(prediction[0])
     return row
 
 st.button('Рассчитать стоимость', on_click = predict)
