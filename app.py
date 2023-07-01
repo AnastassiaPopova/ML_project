f488523b0ebf24dfbf118758c92635b7839b35f5
AnastassiaPopova <135708120+AnastassiaPopova@users.noreply.github.com>
2023-07-02 00:53:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -98,5 +98,7 @@ div.stButton > button:first-child {
 }
 <style>""", unsafe_allow_html=True)
 
-if st.button('Рассчитать стоимость', on_click = predict):
-    st.success('Welcome', np.exp(prediction)[0])
+#if st.button('Рассчитать стоимость', on_click = predict):
+#    st.success('Welcome', np.exp(prediction)[0])
+if st.button('Рассчитать стоимость'):
+    st.success('Welcome', predict)
